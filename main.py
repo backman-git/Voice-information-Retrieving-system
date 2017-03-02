352c347cf689865d2636e822c98571e523775926
Backman <backman.only@gmail.com>
2017-03-02 14:16:44 +0800

    remove voiceRecognition.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from voiceRecognition import voiceToText
 from pypinyin import pinyin, lazy_pinyin
 import pypinyin
 from enhancer import domainWordsCorrector
+import speech_recognition as sr
+import sys
+
 
 
 '''
@@ -9,17 +11,35 @@ Functional Programming
 
 
 '''
+import time
 
 
 
+def voiceToText():
+	r = sr.Recognizer()
 
+	with sr.Microphone() as source:
+		audio = r.listen(source)
+
+	try:
+		return r.recognize_google(audio,language="zh-TW")
+	except sr.UnknownValueError:
+		print("Google Speech Recognition could not understand audio")
+	except sr.RequestError as e:
+		print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+	return None
+
+
+
+print ("說！")
+sys.stdout.flush()
+time.sleep(1)
 
 
 reText=voiceToText()
 
 print (reText)
-
-
-#print (domainWordsCorrector(reText,70.0))
+print (domainWordsCorrector(reText,70.0))
 
 
--- voiceRecognition.py
+++ voiceRecognition.py
@@ -2,23 +2,15 @@
 import speech_recognition as sr
 
 # obtain audio from the microphone
-r = sr.Recognizer()
+
+import time
 
 
 
-def voiceToText():
-	with sr.Microphone() as source:
-		print("Say something!")
-		
-		audio = r.listen(source)
 
-	try:
-		return r.recognize_google(audio,language="zh-TW")
-	except sr.UnknownValueError:
-		print("Google Speech Recognition could not understand audio")
-	except sr.RequestError as e:
-		print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-	return None
+
+
+
 
 		
